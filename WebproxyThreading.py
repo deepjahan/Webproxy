02dfabec80accee6df2e308f8286c490d0e1456b
Stefano Chiesa <base278@gmail.com>
2015-09-19 04:37:57 +0800

    Add working Threaded Webproxy

--- WebproxyThreading.py
+++ WebproxyThreading.py
@@ -3,6 +3,7 @@ import threading
 import SocketServer
 import sys
 import helper
+import urlparse
 
 PROXY_HOST = 'localhost'
 PROXY_PORT = 3282
@@ -13,8 +14,12 @@ def parseRequest(requestString):
   request = helper.Request(requestString)
 
   # parse host
-  hostUrl = 'http://' + request.headers['host']
-  url = urlparse(hostUrl)
+
+  try:
+    hostUrl = 'http://' + request.headers['host']
+    url = urlparse.urlparse(hostUrl)
+  except:
+    return {}
 
   remote = {}
   # get hostname, port, path, method
@@ -29,16 +34,46 @@ class ProxyRequestHandler(SocketServer.BaseRequestHandler):
 
   def handle(self):
     print "Connected:", self.client_address
-    # Get the http request string
+    # get the http request string
     httpRequest = self.request.recv(BUFFER_SIZE)
-
+    if not httpRequest:
+      self.request.close()
+      return
+    # get information about the request
     requestInfo = parseRequest(httpRequest)
 
-    print requestInfo
+    try:
+      remoteSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      remoteSocket.connect((requestInfo.get('host', ''), requestInfo.get('port', 0)))
+      # send the http request
+      remoteSocket.send(httpRequest)
+
+      # receive data from remote socket
+      while 1:
+        data = remoteSocket.recv(BUFFER_SIZE)
+        if data:
+          self.request.send(data)
+        else:
+          break
+
+      remoteSocket.close()
+      self.request.close()
+
+    except socket.error:
+      # send 502 message
+      if self.request:
+        self.request.send(helper.get502())
+        self.request.close()
+
+      if remoteSocket:
+        remoteSocket.close()
+      
+
 
 if __name__ == '__main__':
   if len(sys.argv) == 2:
     PROXY_PORT = int(sys.argv[1])
 
   server = SocketServer.ThreadingTCPServer((PROXY_HOST, PROXY_PORT), ProxyRequestHandler)
+  server.allow_reuse_address=True
   server.serve_forever()
