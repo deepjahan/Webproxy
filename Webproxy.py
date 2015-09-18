9161d76262be5b3f9ebfe36e231dcf2f16aa8dd8
Stefano Chiesa <base278@gmail.com>
2015-09-18 22:50:31 +0800

    Add simple caching

--- Webproxy.py
+++ Webproxy.py
@@ -1,74 +1,95 @@
 import socket
-import parser
+import helper
 import select
 import time
 import sys
-from urlparse import urlparse
+import Queue
+
 
 PROXY_HOST = 'localhost'
 PROXY_PORT = 3282
 BUFFER_SIZE = 65536
 
-from wsgiref.handlers import format_date_time
-from datetime import datetime
-from time import mktime
+class Cache:
 
-def get502():
-  HTTP_RESPONSE_502 = """HTTP/1.0 502 Bad Gateway
-Date: %s
-Server: Apache/2.2.15 (CentOS)
-X-Powered-By: PHP/5.6.4
-Content-Length: 0
-Connection: close
-Content-Type: text/html; charset=UTF-8
-"""
-  now = datetime.now()
-  stamp = mktime(now.timetuple())
-  return HTTP_RESPONSE_502 % (format_date_time(stamp))
-
-
-
-now = datetime.now()
-stamp = mktime(now.timetuple())
-print format_date_time(stamp) 
+  def __init__(self):
+    self.data = []
+    self.length = 0
+    self.available = False
+    self.content_length = -1
 
 class Proxy:
-  sockets = []
+  input_sockets = []
+  output_sockets = []
+  # list of client socket
   clients = []
+  # socket connection
   forward = {}
+  # remote address of remote socket
+  key = {}
+  # real cache (verified using content length)
+  cache = {}
+  # temp cache (unverified)
+  temp_cache = {}
+  # socket message queue
+  message_queues = {}
+
+  def verifyCached(self, socket):
+    # get remote address of socket
+    remoteAddress = self.key[socket]
+
+    # check if content length matches
+    if self.temp_cache[socket].length == self.temp_cache[socket].content_length:
+      self.cache[remoteAddress] = self.temp_cache[socket]
+    else:
+      self.temp_cache[socket] = None
+      del self.temp_cache[socket]
 
   def __init__(self, host, port):
     self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     self.server.bind((host, port))
     self.server.listen(128)
-    self.sockets.append(self.server)
-
+    self.input_sockets.append(self.server)
+    
   def run(self):
     while 1:
-      readable, writable, exceptional = select.select(self.sockets, [], [])
+      readable, writable, exceptional = select.select(self.input_sockets, self.output_sockets, [])
       for socket in readable:
         self.current_socket = socket
+
         if self.current_socket is self.server:
           # ready to accept new connection
           self.accept_current()
-        elif self.current_socket in self.sockets:
+        elif self.current_socket in self.input_sockets:
+          
           self.data = self.current_socket.recv(BUFFER_SIZE)
           if self.data:
             # current socket has data
             self.receive_current()
-          else:
+          elif self.current_socket not in self.output_sockets:
             # close connection with current socket
-            self.close_current()
-          
+            self.close_current(self.current_socket)
+      
+      for socket in writable:
+        try:
+          # try to get next message
+          next_msg = self.message_queues[socket].get_nowait()
+        except:
+          # no messages waiting so stop checking for writability
+          self.output_sockets.remove(socket)
+          if self.forward[socket] is None:
+            # close current if forward is closed
+            self.close_current(self.current_socket)
+        else:
+          socket.send(next_msg)
 
   def accept_current(self):
     connection, clientAddress = self.current_socket.accept()
-    connection.setblocking(0)
-    print clientAddress, "has connected"
-    self.sockets.append(connection)
+    self.input_sockets.append(connection)
     self.clients.append(connection)
     self.forward[connection] = None;
+    self.message_queues[connection] = Queue.Queue()
 
   def receive_current(self):
 
@@ -77,84 +98,122 @@ class Proxy:
       
       print "\nReceive from client", self.current_socket.getpeername()
       
-      print "------"
-      print self.data
-      print "------\n"
       # parse data as HTTP request
-      request = parser.Request(self.data)
+      request = helper.Request(self.data)
 
       # parse host
       hostUrl = 'http://' + request.headers['host']
       url = urlparse(hostUrl)
 
-      # get hostname and port
+      # get hostname, port, path, method
       remoteHost = url.hostname
       remotePort = url.port or 80
-
-      # remove existing remote socket
+      remotePath = request.path
+      remoteMethod = request.command
+      
+      # use them to identify request cache
+      remoteAddress = (remoteMethod, remoteHost, remotePort, remotePath)
+      
+      # check existing remote socket
       remote_socket = self.forward[self.current_socket]
       
+      # if remote socket exist and different port then remove
       if remote_socket is not None and remote_socket.getpeername() is not (remoteHost, remotePort):
-        self.sockets.remove(remote_socket)
-        remote_socket.close()
-        del self.forward[remote_socket]
-        remote_socket = None
+        self.input_sockets.remove(remote_socket)
+
+      # check if cache available
+      cachedData = self.cache.get(remoteAddress, None)
+
+      # if available, use cache
+      if cachedData:
+        # loop cache, and send them
+        for data in cachedData.data:
+          self.message_queues[self.current_socket].put(data)
+
+        # if not in output list, add current socket
+        if self.current_socket not in self.output_sockets:
+          self.output_sockets.append(self.current_socket)
+        return;
+
 
       if remote_socket is None:
+        # try to create connection to remote server
         try:
           remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
           remote_socket.connect((remoteHost, remotePort))
-          
+          # pass here if remote socket manage to make connection
+          self.message_queues[remote_socket] = Queue.Queue()
+          self.key[remote_socket] = remoteAddress
+          self.temp_cache[remote_socket] = Cache()
+
           # add connection between client and remote socket
           self.forward[self.current_socket] = remote_socket
           self.forward[remote_socket] = self.current_socket
-          self.sockets.append(remote_socket)
+          self.input_sockets.append(remote_socket)
         except:
           # host unreachable, send 502 error message
           remote_socket.close()
-          self.current_socket.send(get502())
-          self.close_current()
+          self.message_queues[self.current_socket].put(helper.get502())
+          
+          # if not in output list, add current socket
+          if self.current_socket not in self.output_sockets:
+            self.output_sockets.append(self.current_socket)
           return;
 
     else:
       # current socket is remote socket
+
+      if self.temp_cache[self.current_socket].length == 0:
+        # this is the first response, try to find content-length
+        response = helper.Response(self.data)
+        if response.getheader('Content-Length'):
+          # content length found, get from header minus first position of \r\n\r\n
+          self.temp_cache[self.current_socket].content_length = int(response.getheader('Content-Length')) + self.data.find('\r\n\r\n') + 4
       
-      print "\nReceive from remote", self.current_socket.getpeername(), ' requested by ', self.forward[self.current_socket].getpeername()
-      """
-      print "------"
-      print self.data
-      print "------\n"
-      """
-      # parse data as HTTP response
-      response = parser.Response(self.data)
+      # add to cache
+      self.temp_cache[self.current_socket].data.append(self.data)
+      self.temp_cache[self.current_socket].length += len(self.data)
 
       # TODO: modify data
 
-    self.forward[self.current_socket].send(self.data)
+    # get forward socket
+    forward_socket = self.forward[self.current_socket]
+    # send data that just received
+    self.message_queues[forward_socket].put(self.data)
+
+    # if not in output list, add forward socket
+    if forward_socket not in self.output_sockets:
+      self.output_sockets.append(forward_socket)
 
 
+  def close_current(self, socket):
 
-  def close_current(self):
-    print self.current_socket.getpeername(), "has disconnected"
-
-    # remove from clients and sockets list
-    self.sockets.remove(self.current_socket)
-    if self.current_socket in self.clients:
-      self.clients.remove(self.current_socket)
-
-    # close current socket
-    self.current_socket.close()
+    if socket not in self.input_sockets:
+      # socket already closed
+      return
     
-    # close remote socket
-    remote_socket = self.forward[self.current_socket]
-    if remote_socket:
-      remote_socket.close()
-      self.sockets.remove(remote_socket)
-      del self.forward[remote_socket]
+    # remove from input list
+    self.input_sockets.remove(socket)
 
-    del self.forward[self.current_socket]
+    if socket not in self.clients:
+      # socket is remote socket, try to verify the temp cache
+      self.temp_cache[socket].available = True
+      self.verifyCached(socket)
 
-
+    # close socket
+    socket.close()
+    # remove message queue
+    del self.message_queues[socket]
+    
+    # get the forward socket
+    forward_socket = self.forward[socket]
+    
+    if forward_socket:
+      # forward socket exists
+      self.forward[forward_socket] = None
+      if forward_socket not in self.clients:
+        # close if forward socket is a remote socket
+        self.close_current(forward_socket)
 
 if __name__ == '__main__':
   if len(sys.argv) == 2:
--- helper.py
+++ helper.py
@@ -1,6 +1,10 @@
 from BaseHTTPServer import BaseHTTPRequestHandler
 from StringIO import StringIO
 from httplib import HTTPResponse
+from urlparse import urlparse
+from wsgiref.handlers import format_date_time
+from datetime import datetime
+from time import mktime
 
 class Request(BaseHTTPRequestHandler):
     def __init__(self, request_text):
@@ -24,6 +28,25 @@ class Response(HTTPResponse):
     source = FakeSocket(response_str)
     HTTPResponse.__init__(self, source)
     self.begin()
+
+def get502():
+  HTTP_RESPONSE_502 = """HTTP/1.0 502 Bad Gateway
+  Date: %s
+  Server: Apache/2.2.15 (CentOS)
+  X-Powered-By: PHP/5.6.4
+  Content-Length: 0
+  Connection: close
+  Content-Type: text/html; charset=UTF-8
+
+  """
+  now = datetime.now()
+  stamp = mktime(now.timetuple())
+  return HTTP_RESPONSE_502 % (format_date_time(stamp))
+
+from urlparse import urlparse
+from wsgiref.handlers import format_date_time
+from datetime import datetime
+from time import mktime
 
 # Request
 # error_code 
